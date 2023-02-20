1ad08be9b3212b9fae80641078419d37206991a9
Priyanshi <ppriyanshiyadav22@gmail.com>
2023-02-20 19:28:54 +0530

    first commit

---
+++ calculator/src/App.js
@@ -0,0 +1,62 @@
+import {
+  useRef,
+  useState
+} from "react";
+import './App.css';
+
+function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] =useState(0);
+
+  const plus =(e) =>{
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+  const minus = (e) => {
+     e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+  const times = (e) => {
+     e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+  const div = (e) => {
+     e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  }
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  };
+
+  return (
+    <div className="App">
+        <div>
+          <h1>Simple Working Calculator</h1>
+        </div>
+        <form>
+          <p ref={resultRef}>{result}</p>
+          <input
+            pattern="[0-9]"
+            ref={inputRef}
+            type="number"
+            placeholder="Type a number"
+          />
+          <button onClick={plus}>add</button>
+          <button onClick={minus}>subtract</button>
+          <button onClick={times}>multiply</button>
+          <button onClick={div}>divide</button>
+          <button onClick={resetInput}>reset input</button>
+          <button onClick={resetResult}>reset result</button>
+        </form>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    int a;
+    cout<<"hello shitty dumbo enter value";
+    cin>>a;
+    cout<<"your value is:"<<a;
+    return 0;
+}
